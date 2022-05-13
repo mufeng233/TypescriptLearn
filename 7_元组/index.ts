f6b3286a6cb5d8ef559ee20f493ddcac8171d430
mufeng2333 <2441260435@qq.com>
2022-05-13 17:44:42 +0800

    元组

---
+++ 7_元组/index.ts
@@ -0,0 +1,7 @@
+const person: [string, string, number] = ["liyi", "teacher", 11];
+
+const persons: [string, string, number][] = [
+  ["liyi", "teacher", 20],
+  ["ximeng", "teacher", 22],
+  ["xie", "teacher", 25],
+];
