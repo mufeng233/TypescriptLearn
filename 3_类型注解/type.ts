0b597a712b8cd1ae9e3beda39c7631c369583041
mufeng2333 <2441260435@qq.com>
2022-05-13 17:43:57 +0800

    类型注解

--- 3_类型注解/type.ts
+++ 3_类型注解/type.ts
@@ -17,3 +17,12 @@ const cat: Cat = {
   name: "小白",
   age: 1,
 };
+
+/**
+ * 数字数组类型
+ * @param {string} title - 标题
+ * @returns {array}
+ */
+type NumberArray = number[];
+
+const numberArray: NumberArray = [1, 1, 4, 5, 1, 4];
